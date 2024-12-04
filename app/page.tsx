ab044ed37a0ef474add4044919eb8baa27f001d7
Ras <siregar.riyando@gmail.com>
2024-12-04 16:20:59 +0800

    Creating UI Layout and Component

--- app/page.tsx
+++ app/page.tsx
@@ -30,10 +30,68 @@ export default function Home() {
       {/* Content */}
       {!account ? (
         <div className="flex flex-col lg:flex-row gap-[30px] mt-[20px] px-[30px] min-h-[80vh] justify-center items-center">
-          <button className="btn btn-primary" onClick={connectWallet}>Connect Wallet</button>
+          <button className="btn btn-primary" onClick={connectWallet}>
+            Connect Wallet
+          </button>
         </div>
       ) : (
-        <div>Dashboard</div>
+        <div className="container mx-auto px-4 py-10">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+            {/* Left Section: Mint and Burn */}
+            <div className="bg-white shadow-lg rounded-lg p-6">
+              {/* Mint Section */}
+              <div className="mb-8">
+                <h2 className="text-lg font-bold mb-4">Base URI</h2>
+                <input
+                  type="text"
+                  placeholder="URI"
+                  // value={mintAmount || ""}
+                  // onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  //   setMintAmount(Number(e.target.value) || 0)
+                  // }
+                  className="input input-bordered w-full mb-4"
+                />
+                <button
+                  className="btn btn-info w-full"
+                  // onClick={mintTokens}
+                >
+                  Submit
+                </button>
+              </div>
+
+              {/* Burn Section */}
+              <div>
+                <h2 className="text-lg font-bold mb-4">Mint NFT</h2>
+                <input
+                  type="text"
+                  placeholder="Address"
+                  // value={burnAmount || ""}
+                  // onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  //   setBurnAmount(Number(e.target.value) || 0)
+                  // }
+                  className="input input-bordered w-full mb-4"
+                />
+                <button
+                  className="btn btn-success w-full"
+                  // onClick={mintTokens}
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+
+            {/* Right Section: Token Balance */}
+            <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center">
+              <p className="text-4xl font-semibold mb-4">
+                {/* {balance || "Loading..."} */}
+              </p>
+              <p className="text-lg font-bold mb-4">Name :</p>
+              <p className="text-lg font-bold mb-4">Symbol :</p>
+              <p className="text-lg font-bold mb-4">Owner :</p>
+              <hr className="w-full border-gray-300" />
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
