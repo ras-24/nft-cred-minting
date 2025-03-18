4546c98ebc01a9873da4f77be6ad9a6acc448c5a
Ras <siregar.riyando@gmail.com>
2025-03-18 22:30:57 +0800

    Adding no-explicit-any and no-unused-vars

--- app/contract.ts
+++ app/contract.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import Web3 from "web3";
 import ABI from "./ABI.json";
 
--- app/global.d.ts
+++ app/global.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // global.d.ts
 interface Window {
   ethereum?: {
